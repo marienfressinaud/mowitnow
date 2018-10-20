76b4af1df8bca08701610d3ac9505fc84bc0b346
Marien Fressinaud <dev@marienfressinaud.fr>
2018-10-20 20:06:25 +0200

    add: Init and show lawn size from instructions

--- README.md
+++ README.md
@@ -18,5 +18,37 @@ Ensuite, exécutez le programme avec :
 $ ./mowitnow.py instructions.sample.txt
 ```
 
-Pour le moment, le programme se contente d'afficher le contenu du fichier ; de
-nouvelles fonctionnalités devraient arriver très prochainement !
+Pour le moment, le programme se contente d'afficher la taille du terrain à
+tondre ; de nouvelles fonctionnalités devraient arriver très prochainement !
+
+## Format du fichier en entrée
+
+Le fichier en entrée doit suivre un format pré-défini.
+
+La première ligne indique la taille de la pelouse à tondre. Elle se compose de
+deux nombres strictement supérieurs à 0. Exemples :
+
+```raw
+# Valeurs correctes
+
+5 5
+42 666
+1 1
+
+# Valeurs incorrectes
+
+0 5
+-1 42
+foo 5
+1
+5 5 5
+```
+
+## Tests
+
+Les tests sont écrits avec [doctest](https://docs.python.org/3/library/doctest.html).
+Voici la commande pour les exécuter :
+
+```console
+$ python3 -m doctest -v mowitnow.py
+```
--- mowitnow.py
+++ mowitnow.py
@@ -5,6 +5,49 @@ import argparse
 import os
 
 
+def get_lawn_size(instruction):
+    """Return the size of the lawn from an instruction.
+
+    Examples:
+
+    >>> get_lawn_size("5 5")
+    (5, 5)
+
+    >>> get_lawn_size("0 0") is None
+    True
+
+    >>> get_lawn_size("-5 5") is None
+    True
+
+    >>> get_lawn_size("5 -5") is None
+    True
+
+    >>> get_lawn_size("5 5 5") is None
+    True
+
+    >>> get_lawn_size("5") is None
+    True
+
+    >>> get_lawn_size("") is None
+    True
+
+    >>> get_lawn_size("foo bar") is None
+    True
+    """
+    try:
+        size = tuple(int(x) for x in instruction.split(" "))
+    except ValueError:
+        return None
+
+    if len(size) != 2:
+        return None
+
+    if size[0] <= 0 or size[1] <= 0:
+        return None
+
+    return size
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="the file containing the instructions")
@@ -17,4 +60,11 @@ if __name__ == "__main__":
     with open(args.filename, "r") as f:
         instructions = f.read()
 
-    print(instructions, end="")
+    instruction_lines = instructions.splitlines()
+
+    lawn_size = get_lawn_size(instruction_lines[0])
+    if lawn_size is None:
+        print("The lawn width and height must be greater than 0.")
+        exit(1)
+
+    print(f"Lawn size is {lawn_size[0]}x{lawn_size[1]}.")
